ce2271c456cc2bcf43f18af2d715a8d4e75b2def
mushroom <mushroom019.coder@gmail.com>
2022-12-17 19:06:13 +0530

    finished styling homesceen

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,51 +1,78 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import uuid from 'react-uuid';
+import Slider from 'react-slick';
 interface PropType {
   products: [{ title: string; url: string[] }];
 }
+
+function SamplePrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div onClick={onClick}>
+      <i className="fa-solid  text-black absolute z-[1000] top-[50%] left-[1rem] hidden sm:block sm:text-5xl fa-circle-chevron-left hover:text-cyan-900/90 duration-300"></i>
+    </div>
+  );
+}
+
+function SampleNextArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div onClick={onClick}>
+      <i className="fa-solid absolute top-[50%] right-[1rem] text-black sm:text-5xl hidden sm:block fa-circle-chevron-right hover:text-cyan-900/90 duration-300"></i>
+    </div>
+  );
+}
+
 export default function HomePage(props: PropType) {
+  const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 500,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
     <div className="w-[100%] ">
       {props.products.map((product) => {
         return (
           <div
             key={uuid()}
-            className="mb-[0.6rem] relative justify-center flex flex-col border-b-2  py-[1rem] border-stone-900 overflow-hidden"
+            className="mb-[0.6rem] relative justify-center flex flex-col border-b-2  py-[1rem] border-stone-900 "
           >
-            {/* div to hold buttons for navigation */}
-            <div className="absolute z-50 top-[50%] text-xl sm:text-5xl px-[0.5rem] text-blue-900 sm:px-[1rem] flex justify-between w-full">
-              <button>
-                <i className="fa-solid fa-circle-chevron-left hover:text-cyan-900/90 duration-300"></i>
-              </button>
-              <button>
-                <i className="fa-solid fa-circle-chevron-right hover:text-cyan-900/90 duration-300"></i>
-              </button>
-            </div>
-            {/* links for the category pages */}
             <Link
               href={'/'}
               className="text-xl text-sky-900 font-bold underline flex items-center playfulFont capitalize mb-[0.3rem]"
             >
               <h1>{product.title}</h1>
             </Link>
-            <div className="flex mx-[0.8rem] min-w-full overflow-hidden relative flex items-center self-center">
+            <Slider {...settings}>
               {product.url.map((img) => {
                 return (
                   <div
                     key={uuid()}
-                    className="relative -z-20 min-w-full sm:min-w-[30rem] sm:mx-[0.5rem] h-[8rem] sm:h-[18rem] "
+                    className="relative sm:mx-[1rem] h-[12rem] sm:h-[18rem]"
                   >
                     <Image
                       src={img}
                       alt=""
                       fill
-                      className="object-cover sm:object-[50%_0%]"
+                      className="object-cover sm:object-[50%_50%]"
                     />
                   </div>
                 );
               })}
-            </div>
+            </Slider>
           </div>
         );
       })}
